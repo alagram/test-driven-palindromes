a99f8d380f75697b6d169a6bfe204ac0bdae0ca2
Albert Agram <albert.agram@gmail.com>
2018-12-28 12:09:10 -0500

    Make all palindrome tests pass

--- palindromes.js
+++ palindromes.js
@@ -1,16 +1,33 @@
 module.exports = str => {
   let l = 0;
   let r = str.length - 1;
-  const regex = /^[a-z0-9]+$/i;
+  const regExp = /^[a-z0-9]+$/i;
+  let lStr = [];
+  let rStr = [];
 
-  while (l < r) {
+  while (l <= r) {
+    while (l < r && !str[l].match(regExp)) {
+      l++;
+    }
+
+    while (l < r && !str[r].match(regExp)) {
+      r--;
+    }
+
     if (str[l].toLowerCase() != str[r].toLowerCase()) {
       return [];
+    }
+
+    if (l != r) {
+      lStr.push(str[l].toLowerCase());
+      rStr.push(str[r].toLowerCase());
+    } else {
+      lStr.push(str[l].toLowerCase());
     }
 
     l++;
     r--;
   }
 
-  return [str.toLowerCase()];
+  return [rStr.join('') + lStr.reverse().join('')];
 };
--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -7,5 +7,17 @@ describe('palindromes()', () => {
   });
   it('returns an empty array when given no palindromes', () => {
     expect(palindromes('tic tac toe')).toEqual([]);
+    expect(palindromes('race a car')).toEqual([]);
+  });
+  it('ignores punctuation', () => {
+    expect(palindromes('yo, banana boy!')).toEqual(['yobananaboy']);
+    expect(palindromes('Was it a car or a cat I saw?')).toEqual([
+      'wasitacaroracatisaw',
+    ]);
+  });
+  it('detects multi-word palindromes', () => {
+    expect(palindromes('A man, a plan, a canal, Panama')).toEqual([
+      'amanaplanacanalpanama',
+    ]);
   });
 });
